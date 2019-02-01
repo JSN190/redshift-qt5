ea51ad4bb495689c215821c0c74e6ee11b181f34
John Su <30964725+JSN190@users.noreply.github.com>
2019-02-01 13:41:15 +0000

    Support enable/disable on systemd

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,7 +22,7 @@ void MainWindow::on_buttonServiceStartStop_clicked()
     QThread* thread = new QThread();
     get_service_status* worker = new get_service_status();
     worker->moveToThread(thread);
-    connect(worker, SIGNAL(service_status_received(bool)), this, SLOT(service_status_received(bool)));
+    connect(worker, SIGNAL(service_status_received(bool)), this, SLOT(service_status_received_startstop(bool)));
     connect(thread, SIGNAL(started()), worker, SLOT(run_get_status()));
     connect(worker, SIGNAL(finished()), thread, SLOT(quit()));
     connect(worker, SIGNAL(finished()), worker, SLOT(deleteLater()));
@@ -31,7 +31,7 @@ void MainWindow::on_buttonServiceStartStop_clicked()
     ui->buttonServiceStartStop->setEnabled(false);
 }
 
-void MainWindow::service_status_received(bool running)
+void MainWindow::service_status_received_startstop(bool running)
 {
     QThread* thread = new QThread();
     get_service_status* worker = new get_service_status();
@@ -50,29 +50,68 @@ void MainWindow::service_status_received(bool running)
 }
 
 void MainWindow::service_start_feedback(bool success) {
-    if (success) {
-        ui->buttonServiceStartStop->setText("Stop");
-    }
+    if (success) ui->buttonServiceStartStop->setText("Stop");
     ui->buttonServiceStartStop->setEnabled(true);
 }
 
 void MainWindow::service_stop_feedback(bool success) {
     if (success) {
         ui->buttonServiceStartStop->setText("Start");
+        ui->buttonServiceEnableDisable->setText(QString("Enable"));
     }
     ui->buttonServiceStartStop->setEnabled(true);
 }
 
-void MainWindow::service_enable_feedback(bool success) {
-    qDebug() << "enable feedback";
-    if (success) {
-        qDebug() << "enable feedback success";
+void MainWindow::on_buttonServiceEnableDisable_clicked()
+{
+    QThread* thread = new QThread();
+    get_service_status* worker = new get_service_status();
+    worker->moveToThread(thread);
+    connect(worker, SIGNAL(service_status_received(bool)),
+            this, SLOT(service_status_received_enabledisable(bool)));
+    connect(thread, SIGNAL(started()), worker, SLOT(run_get_status()));
+    connect(worker, SIGNAL(finished()), thread, SLOT(quit()));
+    connect(worker, SIGNAL(finished()), worker, SLOT(deleteLater()));
+    connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+    thread->start();
+    ui->buttonServiceEnableDisable->setEnabled(false);
+    ui->buttonServiceStartStop->setEnabled(false);
+}
+
+void MainWindow::service_status_received_enabledisable(bool running) {
+    QThread* thread = new QThread();
+    get_service_status* worker = new get_service_status();
+    worker->moveToThread(thread);
+    if (running) {
+        connect(worker, SIGNAL(service_stop(bool)),
+                this, SLOT(service_disable_feedback(bool)));
+        connect(thread, SIGNAL(started()), worker, SLOT(run_stop_service()));
+    } else {
+        connect(worker, SIGNAL(service_start(bool)), this, SLOT(service_enable_feedback(bool)));
+        connect(thread, SIGNAL(started()), worker, SLOT(run_start_service()));
     }
+    connect(worker, SIGNAL(finished()), thread, SLOT(quit()));
+    connect(worker, SIGNAL(finished()), worker, SLOT(deleteLater()));
+    connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+    thread->start();
+}
+
+void MainWindow::service_enable_feedback(bool success) {
+    if (success) {
+        ui->buttonServiceEnableDisable->setText(QString("Disable"));
+        ui->buttonServiceStartStop->setText("Stop");
+    }
+    ui->buttonServiceEnableDisable->setEnabled(true);
+    ui->buttonServiceStartStop->setEnabled(true);
 }
 
 void MainWindow::service_disable_feedback(bool success) {
-    qDebug() << "disable feedback";
     if (success) {
-        qDebug() << "disable feedback success";
+        ui->buttonServiceEnableDisable->setText(QString("Enable"));
+        ui->buttonServiceStartStop->setText("Start");
     }
+    ui->buttonServiceEnableDisable->setEnabled(true);
+    ui->buttonServiceStartStop->setEnabled(true);
 }
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,13 +17,16 @@ public:
 
 private slots:
     void on_buttonServiceStartStop_clicked();
-    // status indicates whether the service is running
-    void service_status_received(bool status);
+    // status indicates whether the service is runn
+    void service_status_received_startstop(bool status);
+    void service_status_received_enabledisable(bool status);
     // status indicates whether the start/stop attempt was successful
     void service_start_feedback(bool status);
     void service_stop_feedback(bool status);
     void service_enable_feedback(bool status);
     void service_disable_feedback(bool status);
+
+    void on_buttonServiceEnableDisable_clicked();
 
 private:
     Ui::MainWindow *ui;
